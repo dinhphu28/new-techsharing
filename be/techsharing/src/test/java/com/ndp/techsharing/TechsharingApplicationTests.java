0e79e22b5e576623cf6cd528fc26e812cb07c017
Anthony <18110175@student.hcmute.edu.vn>
2021-10-29 16:33:01 +0700

    Init BE

---
+++ be/techsharing/src/test/java/com/ndp/techsharing/TechsharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndp.techsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
